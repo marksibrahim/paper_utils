fcd7ad7c71e2844aeae44c95eacac589b6d96b4f
marksibrahim <mark.ibhm@gmail.com>
2021-03-30 22:11:29 -0400

    citation complete with readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # paper_utils
 cite and download academic papers
+
+`python article.py cite [url]`
+
+`python article.py download [url]`
+
+
+For command help, 
+`python article.py --help` 
+
+## Installation
+
+`pip install -r requirements.txt`
+
+
+## Testing
+
+`python -m pytest`
--- article.py
+++ article.py
@@ -1,19 +1,18 @@
 """
 Abstract url: https://arxiv.org/abs/1711.02226
 """
-import argparse
 import arxiv
 import click
 
 DEFAULT_DIR = "~/Downloads"
 
 
-def parse_id(abstract_url: str):
+def parse_id(abstract_url: str) -> str:
     id = abstract_url.split("/")[-1]
     return id
 
 
-def get_paper(abstract_url: str):
+def get_paper(abstract_url: str) -> dict:
     paper_id = parse_id(abstract_url)
     paper = arxiv.query(id_list=[paper_id])[0]
     return paper
@@ -27,24 +26,29 @@ def cli():
 @cli.command()
 @click.argument("abstract_url")
 @click.option("--download_dir", default=DEFAULT_DIR, help="Directory to download pdf")
-def download(abstract_url, download_dir: str):
-    print("dog")
-    return abstract_url
+def download(abstract_url: str, download_dir: str) -> None:
+    paper = get_paper(abstract_url)
+    print(f"saving '{paper.get('title')}'")
+    arxiv.download(
+        paper, dirpath=download_dir, slugify=lambda paper: paper.get("title")
+    )
+    print(f"saved to {download_dir}")
 
-    # paper = get_paper(abstract_url)
-    # print(f"saving '{paper.get('title')}'")
-    # arxiv.download(
-    #     paper, dirpath=download_dir, slugify=lambda paper: paper.get("title")
-    # )
-    # print(f"saved to {download_dir}")
 
-def cite_mla(paper: )
+def cite_mla(paper: dict) -> str:
+    """Formats paper object into MLA"""
+    citation = ", ".join(paper.authors) + "."
+    citation += f' "{paper.title}" {paper.id}'
+    citation += f" ({paper.date[:4]})."
+    return citation
+
 
 @cli.command()
 @click.argument("abstract_url")
-def cite(abstract_url):
+def cite(abstract_url: str) -> str:
     paper = get_paper(abstract_url)
-    return True
+    citation = cite_mla(paper)
+    return citation
 
 
 if __name__ == "__main__":
